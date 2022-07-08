17facc30cce1049ea9a077878fb1e57bb631b6d9
Tommaso Bona <piergeolo@gmail.com>
2022-07-08 16:24:08 +0200

    Update T-Search.sh

--- T-Search.sh
+++ T-Search.sh
@@ -13,4 +13,39 @@ echo -e "$blue                        ➡$cyan [\e[46m4\e[46m] Update script "
 echo -e "$blue                        ➡$cyan [\e[46m5\e[46m] Check out my VPN"
 echo -e "$blue                        ➡$cyan [\e[46m6\e[46m] Report issues"
 echo -e "$blue                        ➡$cyan [\e[46m7\e[46m] Exit"
-
+echo -e "$blue               >＞＞＞＞＞$cyan [SELECT]$blue ＜＜＜＜＜<$blue"
+echo " "
+echo -ne "\e[34m#SELECT OPTION\e[34m: "
+read optnz
+if [ $optnz = "1" ];
+then
+w3m www.google.com
+elif [ $optnz = "2" ];
+then
+w3n duckduckgo
+elif [ $optnz = "3" ];
+then
+am start -a android.intent.action.VIEW -d https://msha.ke/tommasobona/ > /dev/null 2>&1
+banner
+menu
+elif [ $optnz = "4" ];
+then
+upd
+elif [ $optnz = "5" ];
+then
+am start -a android.intent.action.VIEW -d https://www.whispervpn.company.site/ > /dev/null 2>&1
+banner
+menu
+elif [ $optnz = "6" ];
+then
+am start -a android.intent.action.VIEW -d https://github.com/ParzivalHack/doomshell/issues > /dev/null 2>&1
+banner
+menu
+elif [ $optnz = "7" ];
+then
+exit 1
+else
+echo "wrong"
+exit
+fi
+}
