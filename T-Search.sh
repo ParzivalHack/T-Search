03cce2778609abba7a3fe0da6639d2545f22ba67
Tommaso Bona <piergeolo@gmail.com>
2022-07-08 16:03:56 +0200

    Create T-Search.sh

---
+++ T-Search.sh
@@ -0,0 +1,15 @@
+blue="\e[34m"
+cyan="\e[46m"
+banner(){
+echo -e "\e[46m"
+toilet T-Search
+echo -e "
+_____________________________________
+|________________________________|🔎|
+echo -e "$blue                        ➡$cyan [\e[46m1\e[46m] Infect device"
+echo -e "$blue                        ➡$cyan [\e[46m2\e[46m] Save device"
+echo -e "$blue                        ➡$cyan [\e[46m3\e[46m] About me"
+echo -e "$blue                        ➡$cyan [\e[46m4\e[46m] Update script "
+echo -e "$blue                        ➡$cyan [\e[46m5\e[46m] Check out my VPN"
+echo -e "$blue                        ➡$cyan [\e[46m6\e[46m] Report issues"
+echo -e "$blue                        ➡$cyan [\e[46m7\e[46m] Exit"
