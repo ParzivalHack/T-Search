f9403361b1b6f0afed0baeb351ee9a9fece9b37f
Tommaso Bona <piergeolo@gmail.com>
2022-07-08 16:31:59 +0200

    Update T-Search.sh

--- T-Search.sh
+++ T-Search.sh
@@ -46,3 +46,70 @@ echo "wrong"
 exit
 fi
 }
+upd(){
+if [ -d "$HOME/T-Search" ];
+then
+cd $HOME
+rm -rf T-Search
+elif [ -d "$HOME/T-Search" ];
+then
+cd $HOME
+rm -rf T-Search
+else
+echo
+exit 1
+fi
+cd $HOME
+sleep 1
+echo -e "         \e[34mUPDATE IS GOING ON, PLEASE BE PATIENT...!\e[0m"
+echo
+printf "                     \e[34m["
+# While process is running...
+while git clone https://github.com/ParzivalHack/T-Search 2> /dev/null; do 
+    printf  "\e[34m▓▓▓▓▓▓▓▓▓▓▓▓▓\e[0m"
+    sleep 1
+done
+printf "\e[34m]\e[0m"
+echo
+echo
+echo
+printf "\e[34m           UPDATED SUCCESSFULLY TO THE LATEST VERSION!\e[0m"
+sleep 2.0
+clear
+cd $HOME
+cd T-Search
+bash T-Search.sh
+}
+vid(){
+FILE=$HOME/T-Search
+if [ -f "$FILE" ]; then
+pop
+else
+echo
+fi
+}
+pop(){
+clear
+read p
+if [ "$p" = "y" ];
+then
+am start -a android.intent.action.VIEW -d ERROR000 2>/dev/null
+clear
+baner1
+menu
+elif [ "$p" = "t" ];
+then
+clear
+baner1
+menu
+elif [ "$p" = "z" ];
+then
+cd $HOMW/T-Search
+rm noob.noob
+banner
+else
+banner
+exit
+fi
+}
+banner
