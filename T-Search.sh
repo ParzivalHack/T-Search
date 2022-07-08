49edded325b464e66b2f35831d3819cff9c4b846
Tommaso Bona <piergeolo@gmail.com>
2022-07-08 16:19:21 +0200

    Update T-Search.sh

--- T-Search.sh
+++ T-Search.sh
@@ -6,10 +6,11 @@ toilet T-Search
 echo -e "
 _____________________________________
 |________________________________|🔎|
-echo -e "$blue                        ➡$cyan [\e[46m1\e[46m] Infect device"
-echo -e "$blue                        ➡$cyan [\e[46m2\e[46m] Save device"
+echo -e "$blue                        ➡$cyan [\e[46m1\e[46m] Use Google (Suggested)"
+echo -e "$blue                        ➡$cyan [\e[46m2\e[46m] Use DuckDuckGo (Safest Option)"
 echo -e "$blue                        ➡$cyan [\e[46m3\e[46m] About me"
 echo -e "$blue                        ➡$cyan [\e[46m4\e[46m] Update script "
 echo -e "$blue                        ➡$cyan [\e[46m5\e[46m] Check out my VPN"
 echo -e "$blue                        ➡$cyan [\e[46m6\e[46m] Report issues"
 echo -e "$blue                        ➡$cyan [\e[46m7\e[46m] Exit"
+
