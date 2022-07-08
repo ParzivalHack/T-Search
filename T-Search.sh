9be8f5f13126cce163c4d598ec8e9d5d814dd7de
Tommaso Bona <piergeolo@gmail.com>
2022-07-08 16:36:30 +0200

    Update T-Search.sh

--- T-Search.sh
+++ T-Search.sh
@@ -1,19 +1,19 @@
 blue="\e[34m"
-cyan="\e[46m"
+cyan="\e[36m"
 banner(){
-echo -e "\e[46m"
+echo -e "\e[36m"
 toilet T-Search
 echo -e "
 _____________________________________
 |________________________________|🔎|
 "
-echo -e "$blue                        ➡$cyan [\e[46m1\e[46m] Use Google (Suggested)"
-echo -e "$blue                        ➡$cyan [\e[46m2\e[46m] Use DuckDuckGo (Safest Option)"
-echo -e "$blue                        ➡$cyan [\e[46m3\e[46m] About me"
-echo -e "$blue                        ➡$cyan [\e[46m4\e[46m] Update script "
-echo -e "$blue                        ➡$cyan [\e[46m5\e[46m] Check out my VPN"
-echo -e "$blue                        ➡$cyan [\e[46m6\e[46m] Report issues"
-echo -e "$blue                        ➡$cyan [\e[46m7\e[46m] Exit"
+echo -e "$blue                        ➡$cyan [\e[36m1\e[36m] Use Google (Suggested)"
+echo -e "$blue                        ➡$cyan [\e[36m2\e[36m] Use DuckDuckGo (Safest Option)"
+echo -e "$blue                        ➡$cyan [\e[36m3\e[36m] About me"
+echo -e "$blue                        ➡$cyan [\e[36m4\e[36m] Update script "
+echo -e "$blue                        ➡$cyan [\e[36m5\e[36m] Check out my VPN"
+echo -e "$blue                        ➡$cyan [\e[36m6\e[36m] Report issues"
+echo -e "$blue                        ➡$cyan [\e[36m7\e[36m] Exit"
 echo -e "$blue               >＞＞＞＞＞$cyan [SELECT]$blue ＜＜＜＜＜<$blue"
 echo " "
 echo -ne "\e[34m#SELECT OPTION\e[34m: "
